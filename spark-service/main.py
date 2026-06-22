5df1b03f468b4e0b74bb1f08a128bd8ef5878d46
catherinmb2019-tech <catherinmb2019@gmail.com>
2026-06-21 19:40:55 -0600

    cambios para spark

--- spark-service/main.py
+++ spark-service/main.py
@@ -3,16 +3,32 @@ from pyspark.sql import SparkSession
 spark = (
     SparkSession.builder
     .appName("RestaurantAnalytics")
-    .config(
-        "spark.hadoop.hive.metastore.uris",
-        "thrift://hive-metastore:9083"
-    )
-    .config("spark.sql.catalogImplementation", "hive")
+    .config("spark.sql.warehouse.dir", "/opt/hive/data/warehouse")
+    .config("spark.hadoop.hive.metastore.uris", "thrift://hive-metastore:9083")
+    .config("spark.sql.hive.metastore.version", "3.1.3")
+    .config("spark.sql.hive.metastore.jars", "maven")
     .enableHiveSupport()
     .getOrCreate()
 )
 
-spark.sql("SHOW DATABASES").show()
-spark.sql("SHOW TABLES IN restaurant_dw").show()
+print("=== DATABASES ===")
+spark.sql("SHOW DATABASES").show(truncate=False)
 
-spark.stop()
+print("=== TABLES ===")
+spark.sql("SHOW TABLES IN restaurant_dw").show(truncate=False)
+
+print("=== FACT ORDERS ===")
+spark.sql("SELECT COUNT(*) FROM restaurant_dw.fact_orders").show()
+
+print("=== DIM PRODUCT ===")
+spark.sql("SELECT COUNT(*) FROM restaurant_dw.dim_product").show()
+
+print("=== FACT ORDERS SAMPLE ===")
+
+spark.sql("""
+SELECT *
+FROM restaurant_dw.fact_orders
+LIMIT 10
+""").show(truncate=False)
+
+spark.stop() 
